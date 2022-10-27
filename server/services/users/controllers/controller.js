f76be22a07003c30639f04e9daf0ad0a71e51450
yasminnabila <yasmnnabila@gmail.com>
2022-10-27 14:42:05 +0700

    Feat: delete user by ID

--- server/services/users/controllers/controller.js
+++ server/services/users/controllers/controller.js
@@ -2,7 +2,7 @@ const { hashedPassword } = require("../helpers/bcryptjs");
 const User = require("../models/user");
 
 class Controller {
-  static async readAllUsers(req, res, next) {
+  static async readAllUsers(req, res) {
     try {
       let data = await User.findAll();
       res.status(200).json(data);
@@ -12,7 +12,7 @@ class Controller {
     }
   }
 
-  static async readUserById(req, res, next) {
+  static async readUserById(req, res) {
     try {
       const { id } = req.params;
       const data = await User.findById(id);
@@ -23,7 +23,7 @@ class Controller {
     }
   }
 
-  static async createNewUser(req, res, next) {
+  static async createNewUser(req, res) {
     try {
       let { username, email, password, phoneNumber, address, role } = req.body;
       // console.log(req.body);
@@ -43,6 +43,20 @@ class Controller {
       });
     } catch (error) {
       // console.log(error);
+      res.status(500).json(error);
+    }
+  }
+
+  static async deleteUserById(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await User.destroy(id);
+      res.status(200).json({
+        statusCode: 200,
+        message: "User has been deleted successfully",
+      });
+    } catch (error) {
+      console.log(error);
       res.status(500).json(error);
     }
   }
--- server/services/users/routes/index.js
+++ server/services/users/routes/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get("/", Controller.readAllUsers);
 router.get("/:id", Controller.readUserById);
 router.post("/", Controller.createNewUser);
-// router.delete("/:id", Controller.delete);
+router.delete("/:id", Controller.deleteUserById);
 
 module.exports = router;
