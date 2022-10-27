50210b48d1c7fcadfabf06b2e335e10733f060b6
yasminnabila <yasmnnabila@gmail.com>
2022-10-27 10:40:24 +0700

    Setup config mongo conenction

---
+++ server/services/users/config/mongo-connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://yasminnabila:yasmin12345@jakarta.y0bydzc.mongodb.net/test";
+const dbName = "letterboxd-DB";
+const client = new MongoClient(uri);
+let db;
+
+async function connect(params) {
+  try {
+    await client.connect();
+    console.log("connected to mongodb!");
+    const dbConnection = client.db(dbName);
+    db = dbConnection;
+    return dbConnection;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function getDB() {
+  return db;
+}
+
+module.exports = { connect, getDB };
