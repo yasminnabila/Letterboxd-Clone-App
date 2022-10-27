9d93f7e588f5f3e360a0730840c22b5e207bca25
yasminnabila <yasmnnabila@gmail.com>
2022-10-27 11:02:17 +0700

    Setup model user mongoDB

---
+++ server/services/users/models/user.js
@@ -0,0 +1,53 @@
+const { ObjectId } = require("mongodb");
+const { getDB } = require("../config/mongo-connection");
+
+class User {
+  static model() {
+    const db = getDB();
+    return db.Collection("users");
+  }
+
+  static async findAll() {
+    try {
+      const user = this.model();
+      const result = await user.find().toArray();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async findById(id) {
+    try {
+      const user = this.users();
+      const result = await user.findOne({
+        _id: ObjectId(id),
+      });
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async create(dataUser) {
+    try {
+      const user = this.model();
+      const result = await user.insertOne(dataUser);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async destroy(id) {
+    try {
+      const user = this.model();
+      const result = await user.deleteOne({
+        _id: ObjectId(id),
+      });
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
