0c631db3637c7515efcb985957d8f39b7a6fa090
yasminnabila <yasmnnabila@gmail.com>
2022-10-27 17:11:38 +0700

    Remove authz in routers

--- server/services/app/routers/movies.js
+++ server/services/app/routers/movies.js
@@ -1,9 +1,5 @@
 const router = require("express").Router();
 const movieController = require("../controllers/movieController");
-const {
-  AuthorizationAdmin,
-  AuthorizationDelete,
-} = require("../middlewares/Authz");
 
 router.get("/", movieController.readAllMovies);
 router.get("/detail", movieController.readOneMovieBySlug);
@@ -12,8 +8,8 @@ router.get("/genres", movieController.readAllGenres);
 router.post("/genres", movieController.createNewGenre);
 router.get("/casts", movieController.readAllCasts);
 router.get("/:id", movieController.readOneMovieById);
-router.put("/:id", AuthorizationAdmin, movieController.updateMovie);
-router.delete("/:id", AuthorizationDelete, movieController.deleteMovieById);
+router.put("/:id", movieController.updateMovie);
+router.delete("/:id", movieController.deleteMovieById);
 router.put("/genres/:id", movieController.updateGenre);
 router.delete("/genres/:id", movieController.deleteGenreById);
 
--- server/services/app/routers/session.js
+++ server/services/app/routers/session.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
-const { AuthorizationAdmin, Authentication } = require("../middlewares/Authz");
 
 router.post("/login", userController.login);
-router.use(Authentication);
-router.post("/register", AuthorizationAdmin, userController.register);
+router.post("/register", userController.register);
 router.post("/google-login", userController.googleLogIn);
 
 module.exports = router;
