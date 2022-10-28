391458689d5191001d626f9def46f7d0dcbf06d2
yasminnabila <yasmnnabila@gmail.com>
2022-10-28 13:22:13 +0700

    Feat: read all users

--- server/orchestrator-express/controllers/userController.js
+++ server/orchestrator-express/controllers/userController.js
@@ -1,6 +1,31 @@
 const axios = require("axios");
 const redis = require("../config/redis");
 
-class userController {}
+const USER_URL = "http://localhost:4001/users";
+
+class userController {
+  static async readAllUsers(req, res, next) {
+    try {
+      console.log("masuk user controller");
+      const usersCache = await redis.get("users");
+      if (usersCache) {
+        console.log("dari redis");
+        const data = JSON.parse(usersCache);
+        res.status(200).json(data);
+      } else {
+        const { data } = await axios({
+          method: "GET",
+          url: USER_URL,
+        });
+
+        await redis.set("users", JSON.stringify(data));
+        res.status(200).json(data);
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+}
 
 module.exports = userController;
--- server/orchestrator-express/routes/userService.js
+++ server/orchestrator-express/routes/userService.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 
+router.get("/", userController.readAllUsers);
+
 module.exports = router;
