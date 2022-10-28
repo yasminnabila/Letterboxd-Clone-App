4f5e5154bf85c2626c72a5b8d4467b8d8cc4c4a2
yasminnabila <yasmnnabila@gmail.com>
2022-10-28 13:32:45 +0700

    Feat: read by id, delete, create users with redis

--- server/orchestrator-express/controllers/userController.js
+++ server/orchestrator-express/controllers/userController.js
@@ -26,6 +26,62 @@ class userController {
       next(error);
     }
   }
+
+  static async readUserById(req, res, next) {
+    try {
+      const { data } = await axios({
+        method: "GET",
+        url: `${USER_URL}/${req.params.id}`,
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async createNewUser(req, res, next) {
+    try {
+      const { username, email, password, phoneNumber, address, role } =
+        req.body;
+
+      const { data } = await axios({
+        method: "POST",
+        url: USER_URL,
+        data: {
+          username,
+          email,
+          password,
+          role,
+          phoneNumber,
+          address,
+        },
+      });
+
+      redis.del("users");
+      res.status(201).json({
+        statusCode: 200,
+        message: "User has been created successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteUserById(req, res, next) {
+    try {
+      await axios({
+        method: "DELETE",
+        url: `${USER_URL}/${req.params.id}`,
+      });
+      redis.del("users");
+      res.status(200).json({
+        statusCode: 200,
+        message: "User has been deleted successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = userController;
