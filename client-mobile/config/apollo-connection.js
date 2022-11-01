4c3553504a9aee3320e5560672e48216d97af484
yasminnabila <yasmnnabila@gmail.com>
2022-11-01 16:12:14 +0700

    Add env to heroku dashboard

--- client-mobile/config/apollo-connection.js
+++ client-mobile/config/apollo-connection.js
@@ -3,7 +3,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   // uri: "http://localhost:4000/",
 
-  uri: "https://bd4d-139-228-111-125.ap.ngrok.io",
+  // uri: "https://bd4d-139-228-111-125.ap.ngrok.io",
+  uri: "https://letterboxd-server-orchestrator.herokuapp.com",
   cache: new InMemoryCache(),
 });
 
