4862e99ebfb71ecd90bcf6b33068a2034765fac3
yasminnabila <yasmnnabila@gmail.com>
2022-11-01 15:45:52 +0700

    Setup redis for deploy

--- server/orchestrator/config/index.js
+++ server/orchestrator/config/index.js
@@ -2,11 +2,11 @@ const APP_URL = "https://letterboxd-server-app.herokuapp.com/movies";
 const USER_URL = "https://letterboxd-server-users.herokuapp.com/users";
 const Redis = require("ioredis");
 const redis = new Redis({
-  port: 12748, // Redis port
+  port: 12748,
   host: "redis-12748.c1.ap-southeast-1-1.ec2.cloud.redislabs.com",
-  username: "default", //, needs Redis >= 6
-  password: "vL7BdWn0dS9v5yfZPUBFyoRF4z6dQOHK",
-  db: 0, // Defaults to 0
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  db: 0,
 });
 
 module.exports = {
