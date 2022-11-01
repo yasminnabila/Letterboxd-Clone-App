73021cc5a117afb5fe63b2cd6b9f4f45468ad30e
yasminnabila <yasmnnabila@gmail.com>
2022-11-01 14:21:41 +0700

    Setup ENV

--- server/services/users/app.js
+++ server/services/users/app.js
@@ -1,7 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 4001;
+const port = process.env.PORT || 4001;
 const mongoConnection = require("./config/mongo-connection");
 const router = require("./routes");
 const ErrorHandler = require("./middlewares/ErrorHandler");
